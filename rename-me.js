bc68ffa833bee121e8630775d8863bea1021d061
redhed7588 <redhed7588@fullsail.edu>
2012-08-09 22:04:07 -0500

    Update rename-me.js

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,12 @@
-alert("JavaScript works!");
+var kids = ["Jorden,", " Jalen,"];
+var daysPerWeek = 5,
+var fiance = "Cliff"
+
+console.log(daysPerWeek + 'days a week, I make sure' + kids + " and " + fiance + "wake up on time.");
+
+var wakeUp = function(time) {
+  if (time > 6) {console.log("I turned off the alarm and got up.");};
+	else {console.log("I went back to sleep for a little longer.");};
+};	
+	wakeUp(7)
+	
