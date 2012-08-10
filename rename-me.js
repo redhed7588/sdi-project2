d0f16fde5bbc150a68243fdd72dee23e5e8cb3ea
Angela Williams <redhed7588@fullsail.edu>
2012-08-09 22:27:31 -0500

    second commit

--- rename-me.js
+++ rename-me.js
@@ -5,8 +5,16 @@ var fiance = "Cliff"
 console.log(daysPerWeek + 'days a week, I make sure' + kids + " and " + fiance + "wake up on time.");
 
 var wakeUp = function(time) {
-  if (time > 6) {console.log("I turned off the alarm and got up.");};
+	if (time > 6) {console.log("I turned off the alarm and got up.");};
 	else {console.log("I went back to sleep for a little longer.");};
 };	
 	wakeUp(7)
 	
+var getCoffee = function (cups, type) {
+	var stillTired = true;
+		if (stilleTired = false) {console.log("I decide to skip the coffee.");
+		else {console.log("I made " + cups + " cups" + " of " + type + " coffee.");}:
+	return stillTired;
+};
+
+var get Cliff =
