70d44b20464f39c2035103311d9bed7dd11fe5c8
Angela Williams <redhed7588@fullsail.edu>
2012-08-09 22:57:12 -0500

    last minute changes

--- rename-me.js
+++ rename-me.js
@@ -5,20 +5,24 @@ var fiance = "Cliff"
 console.log(daysPerWeek + 'days a week, I make sure' + kids + " and " + fiance + "wake up on time.");
 
 var wakeUp = function(time) {
-	if (time > 6) {console.log("I turned off the alarm and got up.");};
+	if (time > 6) {
+	console.log("I turned off the alarm and got up.");};
 	else {console.log("I went back to sleep for a little longer.");};
 };	
-	wakeUp(7)
+
+wakeUp(7);
 	
 var getCoffee = function (cups, type) {
 	var stillTired = true;
-		if (stilleTired = false) {console.log("I decide to skip the coffee.");
+		if (stillTired = false) {console.log("I decide to skip the coffee.");};
 		else {console.log("I made " + cups + " cups" + " of " + type + " coffee.");}:
 	return stillTired;
 };
 
+getCoffee(4,Colombian);
+
 var getCliff = function (timeLeft) {
-		while (timeLeft>4) {console.log("Cliff, you have " + timeLeft + " minutes to get up.")
+		while (timeLeft>4) {console.log("Cliff, you have " + timeLeft + " minutes to get up.");
 		timeLeft -= 5;}
 		console.log("You need to get up now!");
 		return timeLeft;
@@ -40,6 +44,8 @@ var getKidsReady = function (activity, minutesLeft) {
 			console.log("You have " + time + " minutes left to " + activity + ".");};
 	return time
 };
+
+console.log("Ahhhh...Peace and quiet!");
 
 
 			
